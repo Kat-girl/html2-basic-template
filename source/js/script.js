c3c0c314609480603d5a5e24e725394b04caf407
Kat-girl <podkaterina@gmail.com>
2023-11-02 09:39:12 +0300

    Добавлен скрипт для открытия-закрытия меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMenuButtons = document.querySelectorAll('.navigation__menu-button');
+const navigationMenu = document.querySelector('.navigation__menu');
+const navMenuButtonClosed = document.querySelector('.navigation__menu-button--closed');
+const navMenuButtonOpened = document.querySelector('.navigation__menu-button--opened');
+
+navMenuButtons.forEach((navMenuButton) => {
+  navMenuButton.addEventListener('click', () => {
+    navigationMenu.classList.toggle('navigation__menu--opened');
+    navMenuButtonClosed.classList.toggle('hide');
+    navMenuButtonOpened.classList.toggle('hide');
+  });
+});
+
+
