386894827893ae2510b609fe84107279f14d55ed
Kat-girl <podkaterina@gmail.com>
2023-11-03 09:30:09 +0300

    Добавлены состояния для кнопок и ссылок

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('.header');
 const navMenuButtons = document.querySelectorAll('.navigation__menu-button');
 const navigationMenu = document.querySelector('.navigation__menu');
 const navMenuButtonClosed = document.querySelector('.navigation__menu-button--closed');
@@ -5,6 +6,7 @@ const navMenuButtonOpened = document.querySelector('.navigation__menu-button--op
 
 navMenuButtons.forEach((navMenuButton) => {
   navMenuButton.addEventListener('click', () => {
+    header.classList.toggle('header--menu-opened');
     navigationMenu.classList.toggle('navigation__menu--opened');
     navMenuButtonClosed.classList.toggle('hide');
     navMenuButtonOpened.classList.toggle('hide');
