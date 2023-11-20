39d1e898ea04a61cfb3c8c2942a5d54e4659a30a
Kat-girl <podkaterina@gmail.com>
2023-11-20 15:27:25 +0300

    Добавлена фавиконка; картинка карты без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,13 @@
 const header = document.querySelector('.header');
 const navMenuButton = document.querySelector('.navigation__menu-button');
 const navigation = document.querySelector('.navigation');
+const mapContainer = document.querySelector('.contacts__map-container');
+const mapPicture = mapContainer.querySelector('.contacts__map-picture');
 
 header.classList.remove('header--nojs');
 navigation.classList.remove('navigation--nojs');
+mapContainer.classList.remove('contacts__map-container--nojs');
+mapPicture.classList.add('hide');
 
 
 navMenuButton.addEventListener('click', () => {
